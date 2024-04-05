588cd0cd0ef3c80bc3e38cb8aa767395f5e2af99
Praveen <praveenworkforum@gmail.com>
2024-04-05 16:52:54 +0530

    h

---
+++ chec.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include<string>
+int main()
+{
+    char str='H', d='h';
+    // cout<<str.substr(2)<<endl;
+    // cout<<((d-'0')-(str-'0'))<<endl;
+    cout<<abs((str-'0')-(d-'0'))<<endl;
+    // cout<<str;
+    if(isupper(str))
+    cout<<"Yes";
+    else
+    cout<<"No";
+   
+   return 0;
+}
